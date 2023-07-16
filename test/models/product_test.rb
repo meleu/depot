994c125e9c510a93c9983e8a7f8946a09e7350f7
Augusto Lopes <meleu.dev@gmail.com>
2023-07-15 23:08:01 -0300

    add tests and validations for Product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,63 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test 'price must be positive' do
+    product = products(:valid_product)
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test 'image url' do
+    product = products(:valid_product)
+
+    good_names = %w[
+      img.gif img.jpg img.png IMG.JPG IMG.Jpg http://a.b.c/x/y/z/img.gif
+    ]
+    bad_names = %w[
+      img.doc img.gif/more img.gif.more
+    ]
+
+    good_names.each do |img_url|
+      product.image_url = img_url
+      assert product.valid?, "#{img_url} must be valid"
+    end
+
+    bad_names.each do |img_url|
+      product.image_url = img_url
+      assert product.invalid?, "#{img_url} must be invalid"
+    end
+  end
+
+  test 'title is unique' do
+    product = products(:ruby)
+    title = product.title
+
+    new_product = Product.new(
+      title:,
+      description: 'New Description',
+      image_url: 'something.jpg',
+      price: 9.99
+    )
+
+    assert new_product.invalid?
+    assert_equal error_messages, new_product.errors[:title]
+  end
 end
