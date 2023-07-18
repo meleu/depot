87b6bbc09ecc9d9c0c63c3e9e52a7fd50c874e3b
Augusto Lopes <meleu.dev@gmail.com>
2023-07-18 09:36:06 -0300

    stronger test for store#index

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,8 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 3 # the amount of products in the fixture
+    assert_select 'h2', 'Programming Ruby' # one of the products in the fixture
+    assert_select 'div', /\$[,\d]+\.\d\d/
   end
 end
