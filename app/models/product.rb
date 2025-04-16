2736ab33dbc730c183262849a79860614266a772
meleu <meleu.dev@gmail.com>
2025-04-16 04:24:24 -0300

    Add Turbo Streams for real-time product updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  # TODO: (meleu) have a better understanding about this.
+  # broadcast changes to the product model to any clients that are listening
+  after_commit -> { broadcast_refresh_later_to "products" }
 end
